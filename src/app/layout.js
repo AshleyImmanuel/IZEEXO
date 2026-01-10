1d3e893b893859f0d9f13affe64a0dbcee85eb77
Ashley Immanuel <immmanuelashley77@gmail.com>
2026-01-10 21:18:15 +0530

    fix: replace css imports with layout imports to resolve nextjs warning

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@/styles/auth.css";
+import "@/styles/utilities.css";
 import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
